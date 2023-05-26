7fa15bcb2374ce0b997c88683983e749eb89b86b
kaiquePMS <kaiquepinguim3@gmail.com>
2023-05-26 09:57:38 -0300

    animal,main,pessoa

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    String especie;
+    String porte;
+    int idade;
+    String raca;
+    String corpelo;
+    String cor0lhoDireito;
+    String nome;
+    //Metodos
+    public void comer(){
+        System.out.println("Animal comeu");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-  // Aula introdutiva de Orientação á objetos
+        // Aula introdutiva de Orientação á objetos
+
+        // Aula introdutória de Orientação à Objetos
+        // Criar objetos
+        Pessoa adao = new Pessoa();
+        // Declaração de objeto
+        Pessoa qualquer;
+        // Instanciação do objeto
+        qualquer = new Pessoa();
+        Pessoa adulta = new Pessoa();
+        // Definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        // Definir comportamento do objeto
+        qualquer.falar();
+        adao.falar("baixo");
+        // Criar mais objetos
+        // Declarar objeto
+        Pessoa rainha;
+        // Instanciar objetos
+        rainha = new Pessoa();
+        // Definir formato do objeto
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+        // Definir comportamento
+        rainha.falar();
+        rainha.falar("alto");
+        System.out.println(rainha.falar("alto", "grave"));
+
+        Animal cao = new Animal();
+        cao.nome = "Pluto";
+        cao.comer();
+
+        rainha.comer();
     }
 }
